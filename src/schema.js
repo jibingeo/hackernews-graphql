7e46c5ab316051073c9b46d6cddee95b715bbece
Jibin George <mail@jibingeorge.com>
2018-02-19 13:34:00 +0530

    added mutation

--- src/schema.js
+++ src/schema.js
@@ -2,84 +2,27 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { getItem, getTopStories, getUser } from './api';
 
 
+
+let data = ['hello']
+
 const typeDefs =  /* GraphQL */`
-  # the schema allows the following query:
   type Query {
-    topStories(first: Int): [Story]
-    story(id: ID!): Story
-    user(id: String!): User
+    hello: [String]
   }
-
-  # represent Story
-  type Story {
-    id: ID!,
-    title: String,
-    url: String,
-    comments: [Comment]
-    user: User
+  type Mutation {
+    addItem(item: String): [String]
   }
-
-  # represent Comment
-  type Comment {
-    id: ID!,
-    text: String,
-    comments: [Comment]
-    user: User
-  }
-
-  type User {
-    id: ID!,
-    about: String,
-    karma: Int
-    items: [UserItems]
-  }
-
-  union UserItems = Comment | Story | Job
-
-  type Job {
-    id: ID!,
-    title: String,
-    url: String,
-    user: User
-  }
-
 `;
 
 const resolvers = {
   Query: {
-    topStories: async (_, {first=10}) => {
-      let list = await getTopStories()
-      list = list.slice(0, first);
-      return list.map( id => getItem(id))
-    },
-    story: (_, {id}) => getItem(id),
-    user: (_, { id }) => getUser(id)
+    hello: () => data
   },
-  Story: {
-    comments: (story) => {
-      let kids = story.kids || []
-      return kids.map(id => getItem(id))
-    },
-    user: (story) => getUser(story.by),
-    __isTypeOf: (comment) => comment.type === 'story'
-  },
-  Comment: {
-    comments: (comment) => {
-      let kids = comment.kids || []
-      return kids.map(id => getItem(id))
-    },
-    user: (comment) => getUser(comment.by),
-    __isTypeOf: (comment) => comment.type === 'comment'
-  },
-  User: {
-    items: async (user) => {
-      let list = user.submitted.slice(0, 10);
-      return list.map( id => getItem(id))
+  Mutation: {
+    addItem: (_, {item}) => {
+      data = [...data, item]
+      return data
     }
-  },
-  Job: {
-    user: (job) => getUser(job.by),
-    __isTypeOf: (comment) => comment.type === 'job'
   }
 };
 
